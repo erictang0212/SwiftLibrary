141a7be2767dc72f7d2b88743accb18d43190751
EricTang <erictangappdev@gmail.com>
2023-04-19 23:10:51 +0800

    Add constraintEqualToSuperView for projects not using SnapKit.

--- SwiftLibrary/SwiftLibrary/Extension/UIView+Extension.swift
+++ SwiftLibrary/SwiftLibrary/Extension/UIView+Extension.swift
@@ -16,4 +16,14 @@ extension UIView {
         view.frame = bounds
         addSubview(view)
     }
+    
+    // MARK: - Constraint
+    func constraintEqualToSuperView() {
+        guard let superview = superview else { return }
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
+        self.bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
+        self.leadingAnchor.constraint(equalTo: superview.leadingAnchor).isActive = true
+        self.trailingAnchor.constraint(equalTo: superview.trailingAnchor).isActive = true
+    }
 }
