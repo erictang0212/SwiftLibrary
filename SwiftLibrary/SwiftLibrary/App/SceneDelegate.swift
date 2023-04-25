e1980f3f6ccfec58ca471d05156081534c96c7bc
EricTang <erictangappdev@gmail.com>
2023-04-25 10:16:58 +0800

    Remove unused code

--- SwiftLibrary/SwiftLibrary/App/SceneDelegate.swift
+++ SwiftLibrary/SwiftLibrary/App/SceneDelegate.swift
@@ -12,17 +12,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        registerDependenies()
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let mainCoordinator = MainCoordinator(window: window!,
                                               navigationController: UINavigationController())
         mainCoordinator.start()
         window?.makeKeyAndVisible()
-    }
-    
-    private func registerDependenies() {
-        DependencyContainer.shared.register(RootViewController())
     }
 }
 
